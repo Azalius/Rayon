bf870fdd61750e3f3c109e6e4a24277b5ded2ad6
Azalius <mateo.enzo@yahoo.fr>
2018-02-13 21:44:36 +0100

    vecteur OK des test a reajouter et a faire passer

--- bases3d.cpp
+++ bases3d.cpp
@@ -114,15 +114,40 @@ Vecteur3D Vecteur3D::Reflechir(const Vecteur3D & n) const { //TODO
 }
 
 Vecteur3D Vecteur3D::Refracter(const Vecteur3D & norm, float m1, float m2) const { // TODO
-	Vecteur3D res;
+
+    Vecteur3D c1;
+    Vecteur3D c2;
+    Vecteur3D n = Vecteur3D(norm.X(), norm.Y(), norm.Z());
+
+    if (n*(*this) < 0){
+        n = -n;
+    }
+
 	float coef = m1/m2;
-    float oldAngle = acos(((*this)*norm)/(Longueur()*norm.Longueur())); // signe?
+    float oldAngle = acos(((*this)*n)/(Longueur()*n.Longueur())); // signe?
     if (sin(oldAngle) > 1/coef){ // refraction totale, mb?
         return Vecteur3D(0,0,0);
     }
     float newAngle = asin(sin(oldAngle) / coef);
 
-    res = sin(newAngle) * norm;
-	return res;
+    c1 = ((*this)+n*cos(oldAngle))*sin(newAngle);
+    c2 = -n*cos(newAngle);
+
+	return c1 + c2;
 }
+/*
+Vec3f refract(const Vec3f &I, const Vec3f &N, const float &ior)
+{
+    float cosi = clamp(-1, 1, I.dotProduct(N));
+    float etai = 1, etat = ior;
+    Vec3f n = N;
+    if (cosi < 0) { cosi = -cosi; } else { std::swap(etai, etat); n= -N; }
+    float eta = etai / etat;
+    float k = 1 - eta * eta * (1 - cosi * cosi);
+    return k < 0 ? 0 : eta * I + (eta * cosi - sqrtf(k)) * n;
+}
+ */
+
+
+
 
--- tests/testVec.cpp
+++ tests/testVec.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "bases3d.hpp"
+#include "../bases3d.hpp"
 
 using namespace std;
 
@@ -36,9 +36,22 @@ int main() {
 		cout << "-2" << endl;
 	}
 	if (!(p1 - p2 == Point3D(1.0, -3.8, 10.3))) {
-		cout << "+5" << endl;
+		cout << "-3" << endl;
 	}
 
+    if(!(v1*v2) == -92.0/5.0){
+        cout << "*" <<endl;
+    }
 
 
+
+    v3.Normaliser();
+    v4.Normaliser();
+    float lv4 = 3.61109402;
+    if (!(v3 == Vecteur3D(0.0, 0.0, 1.0))){
+        cout<<"n1"<<endl;
+    }
+    if (!(v4 == Vecteur3D(-3.0/lv4, -2.0/lv4, 0.2/lv4))){
+        cout<<"n2"<<endl;
+    }
 }
